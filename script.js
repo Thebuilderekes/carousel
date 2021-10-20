fee681127f95a8ddef333abdd54cc18e7e7ac886
Ekeopre Beredugo <68848865+Thebuilderekes@users.noreply.github.com>
2021-10-20 11:56:55 +0000

    carousel visibility on slider button click

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const slides = document.getElementsByClassName('carousel-item');
 let slidePosition = 0;
 const totalSlides = slides.length;
@@ -5,10 +6,25 @@ const totalSlides = slides.length;
 document.getElementById('carousel-button-next').addEventListener('click', moveToNextSlide);
 document.getElementById('carousel-button-prev').addEventListener('click', moveToPrevSlide);
 
+function hideAllSlides() {
+    for (let slide of slides) {
+        slide.classList.remove('carousel-item-visible');
+        slide.classList.add('carousel-item-hidden');
+    }
+}
+
 function moveToNextSlide() {
-    console.log('moving to the next slide!');
+    hideAllSlides();
+    
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+    
+    slides[slidePosition].classList.add("carousel-item-visible");
 }
 
 function moveToPrevSlide() {
-    console.log('moving to the prev slide!');    
+    
 }
